00e7286a482539326282f3ac13c1ef4eab7d9155
huskatep <huskatep@gmail.com>
2026-04-26 16:14:38 +0300

    Add News page route to App component

--- artifacts/steve-analiz/src/App.tsx
+++ artifacts/steve-analiz/src/App.tsx
@@ -3,7 +3,6 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import NotFound from "@/pages/not-found";
-
 import { AppLayout } from "@/components/layout/AppLayout";
 import Home from "@/pages/Home";
 import Blog from "@/pages/Blog";
@@ -14,6 +13,7 @@ import Writer from "@/pages/Writer";
 import Vision from "@/pages/Vision";
 import Watchlist from "@/pages/Watchlist";
 import SteveAnalizAI from "@/pages/SteveAnalizAI";
+import News from "@/pages/News";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -31,6 +31,7 @@ function Router() {
         <Route path="/" component={Home} />
         <Route path="/blog" component={Blog} />
         <Route path="/blog/:slug" component={BlogPostDetail} />
+        <Route path="/news" component={News} />
         <Route path="/education" component={Education} />
         <Route path="/education/:slug" component={EducationDetail} />
         <Route path="/writer" component={Writer} />
